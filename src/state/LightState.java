aa9e08a83338a98e9881e1950f01b0e413cf6712
ssyup4259 <ssyup8717@gmail.com>
2020-04-21 13:36:49 +0900

    second commit

---
+++ src/state/LightState.java
@@ -0,0 +1,5 @@
+package state;
+public interface LightState {
+	public void on_button_pushed(Light light);
+	public void off_button_pushed(Light light);
+}
---
+++ src/state/OFF.java
@@ -0,0 +1,19 @@
+package state;
+public class OFF implements LightState {
+
+	private static LightState instance = new OFF();
+	
+	public static LightState getInstance() {
+		return instance;
+	}
+	@Override
+	public void on_button_pushed(Light light) {
+		System.out.println("Light On!!");
+		light.setState(ON.getInstance());
+	}
+	@Override
+	public void off_button_pushed(Light light) {
+		System.out.println("반응 없음");
+		light.setState(OFF.getInstance());
+	}
+}
